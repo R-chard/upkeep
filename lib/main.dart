4650f1c59aeb853bce620a1ec93f501e5140ca69
Akanksha1212 <akankshasingh121297@gmail.com>
2021-01-23 22:13:19 +0530

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'screens/splashscreen.dart';
+import 'package:flutter/services.dart';
+
+import 'screens/landingScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +14,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LandingScreen()));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                'assets/images/logo.png',
+                height: 600,
+                width: 600,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

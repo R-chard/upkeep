49346b30b7b9256645ccaad567254c37761cfbc9
Akash S M <akashsm62@gmail.com>
2021-01-23 19:52:34 +0530

    Home and Splash Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,21 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splash(),
-    );
-  }
-}
-
-class Splash extends StatefulWidget {
-  @override
-  _SplashState createState() => _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(child: Text("hi")),
+      home: SplashScreen(),
     );
   }
 }

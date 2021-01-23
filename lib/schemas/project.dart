8da0239b8c89b20e586a394f17c9080e67dbb255
richard <richardding1999@gmail.com>
2021-01-23 13:10:49 +0000

    Synced to firebase and added schemas

---
+++ lib/schemas/project.dart
@@ -0,0 +1,9 @@
+class Project{
+  String title;
+  String description;
+  int fundRequired;
+  int currentFund;
+  // ToDo: Add Image
+
+  Project(this.title,this.description,this.fundRequired,this.currentFund);
+}
---
+++ lib/schemas/user.dart
@@ -0,0 +1,9 @@
+class User{
+  String username;
+  // ToDo: Hash password
+  String password;
+  String city;
+  String email;
+
+  User(this.username,this.password,this.city,this.email);
+}
